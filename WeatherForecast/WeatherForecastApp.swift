88ecb1b826c3ad5e20f3a8b2f8c8cbf662ab4d55
Rishab Maharjan <gunnerrishav@gmail.com>
2024-07-28 13:59:24 +0545

    Initial Commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Rishab Maharjan on 28/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
